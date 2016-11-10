794003b5ac08bee66bc86f1ae01ca94a5edb9fea
Zayn <zchen35@stevens.edu>
2016-11-09 19:36:33 -0500

    UI part
    Upload UI part, if want to change windows, just change the
    ui_mainwindow.h file

---
+++ UI/810c/mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextEdit>
+#include <QColorDialog>
+#include <QAction>
+#include <QFontDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    //ui->textEdit->
+}
+
+void MainWindow::on_actionFont_triggered()
+{
+    bool ok;
+    QFont font=QFontDialog::getFont(&ok,this);
+}
+
+void MainWindow::on_actionColor_triggered()
+{
+    QColorDialog  color(this);
+    color.setOption(QColorDialog::ShowAlphaChannel);
+    color.exec();
+    QColor color1=color.currentColor();
+}
---
+++ UI/build-810c-Desktop_Qt_5_7_0_clang_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,115 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionFont;
+    QAction *actionColor;
+    QWidget *centralWidget;
+    QFrame *frame_1;
+    QTextBrowser *textBrowser;
+    QFrame *frame_2;
+    QTextBrowser *textBrowser_2;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(590, 1265);
+        actionFont = new QAction(MainWindow);
+        actionFont->setObjectName(QStringLiteral("actionFont"));
+        actionColor = new QAction(MainWindow);
+        actionColor->setObjectName(QStringLiteral("actionColor"));
+        actionColor->setVisible(true);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        frame_1 = new QFrame(centralWidget);
+        frame_1->setObjectName(QStringLiteral("frame_1"));
+        frame_1->setGeometry(QRect(40, 20, 511, 291));
+        frame_1->setFrameShape(QFrame::StyledPanel);
+        frame_1->setFrameShadow(QFrame::Raised);
+        textBrowser = new QTextBrowser(frame_1);
+        textBrowser->setObjectName(QStringLiteral("textBrowser"));
+        textBrowser->setGeometry(QRect(0, 0, 511, 291));
+        frame_2 = new QFrame(centralWidget);
+        frame_2->setObjectName(QStringLiteral("frame_2"));
+        frame_2->setGeometry(QRect(40, 340, 511, 291));
+        frame_2->setFrameShape(QFrame::StyledPanel);
+        frame_2->setFrameShadow(QFrame::Raised);
+        textBrowser_2 = new QTextBrowser(frame_2);
+        textBrowser_2->setObjectName(QStringLiteral("textBrowser_2"));
+        textBrowser_2->setGeometry(QRect(0, 0, 511, 291));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 590, 22));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionFont);
+        menuFile->addAction(actionColor);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        actionFont->setText(QApplication::translate("MainWindow", "font", 0));
+        actionColor->setText(QApplication::translate("MainWindow", "color", 0));
+        textBrowser->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'.Helvetica Neue DeskInterface'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">A<span style=\" font-size:144pt;\">AS</span><span style=\" font-size:144pt; color:#ff4b40;\">D</span></p></body></html>", 0));
+        menuFile->setTitle(QApplication::translate("MainWindow", "file", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
